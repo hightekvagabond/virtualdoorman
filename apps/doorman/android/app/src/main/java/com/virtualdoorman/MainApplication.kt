6cb9c0da18901e7138a642a0b1bc15abe6a03100
Gypsy Rogers <github@gypsy.org>
2026-08-27 06:36:35 -0700

    feat(kiosk): MainApplication – register KioskPackage

--- apps/doorman/android/app/src/main/java/com/virtualdoorman/MainApplication.kt
+++ apps/doorman/android/app/src/main/java/com/virtualdoorman/MainApplication.kt
@@ -6,22 +6,23 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.virtualdoorman.kiosk.KioskPackage
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactHost: ReactHost by lazy {
-    getDefaultReactHost(
-      context = applicationContext,
-      packageList =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
-        },
-    )
-  }
+    override val reactHost: ReactHost by lazy {
+        getDefaultReactHost(
+            context = applicationContext,
+            packageList =
+                PackageList(this).packages.apply {
+                    // In-tree modules not picked up by autolink:
+                    add(KioskPackage())
+                },
+        )
+    }
 
-  override fun onCreate() {
-    super.onCreate()
-    loadReactNative(this)
-  }
+    override fun onCreate() {
+        super.onCreate()
+        loadReactNative(this)
+    }
 }
